044383ef5c04b1f811d83aec2e549382eae211ef
Umair-xdd <m.umairwaheedansari@gmail.com>
2024-10-22 13:14:21 +0500

    Updated the layout and fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -48,18 +48,18 @@ function App() {
             {/* No Button */}
           </div>
         </div>
+        <button
+          className="bg-red-500 text-white px-8 py-2 rounded-full text-xl transition-all mt-5"
+          style={{
+            position: "absolute",
+            top: noButtonPosition.top,
+            left: noButtonPosition.left,
+          }}
+          onMouseEnter={moveNoButton}
+        >
+          No
+        </button>
       </div>
-      <button
-        className="bg-red-500 text-white px-8 py-2 rounded-full text-xl transition-all mt-5"
-        style={{
-          position: "absolute",
-          top: noButtonPosition.top,
-          left: noButtonPosition.left,
-        }}
-        onMouseEnter={moveNoButton}
-      >
-        No
-      </button>
 
       {showResult && (
         <div className="absolute text-center text-4xl text-pink-600 font-bold top-20">
